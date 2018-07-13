c22488077104d94f03d3d3ce0b58cf91d3262e4a
andy <andy_villalobos90@yahoo.com>
2018-07-13 11:20:44 -0700

    ready for awesome

--- Server/index.js
+++ Server/index.js
@@ -21,7 +21,7 @@ app.use(function (req, res, next) {
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requestedß-With,content-type');
+  res.setHeader('Access-Control-Allow-heroku Headers', 'X-Requestedß-With,content-type');
   
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
